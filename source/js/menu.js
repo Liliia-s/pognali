1258a126bac51d99354df5ef22fa8559236af25d
Liliia <befolife@gmail.com>
2020-09-03 22:18:57 +0300

    Внесла правки

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,27 +2,30 @@
 
 (function () {
   var itemsOfMenu = document.querySelectorAll('.main-nav__menu, .main-nav__auth, .main-nav__contacts, .main-nav__social');
-  var mainNavAuth = document.querySelector('.main-nav__auth');
-  var windowInnerWidth = window.innerWidth;
-  var windowInnerClientWidth = document.body.clientWidth;
-  var screenWidth = window.screen.width;
-  var windowOuterWidth = window.outerWidth;
+  var menuButton = document.querySelector('.main-nav__toggle');
+  // var mainNavAuth = document.querySelector('.main-nav__auth');
+  // var windowInnerWidth = window.innerWidth;
+  // var windowInnerClientWidth = document.body.clientWidth;
+  // var screenWidth = window.screen.width;
+  // var windowOuterWidth = window.outerWidth;
 
   Array.prototype.slice.call(itemsOfMenu);
 
-  var hideItemsOfMenu = function () {
-    if ((windowInnerWidth < 768) || (windowInnerClientWidth < 768)) {
-      itemsOfMenu.forEach(function (item) {
-        item.classList.add('display-none');
-      });
-    } if ((windowInnerWidth > 767) || (windowInnerClientWidth > 767)) {
-      mainNavAuth.classList.remove('display-none');
-    } if ((windowInnerWidth > 1439) || (screenWidth > 1439)) {
-      itemsOfMenu.forEach(function (item) {
-        item.classList.remove('display-none');
-      });
-    }
-  };
+  // var hideItemsOfMenu = function () {
+  itemsOfMenu.forEach(function (item) {
+    item.classList.toggle('display-none');
+  });
+  // };
 
-  hideItemsOfMenu();
+  // hideItemsOfMenu();
+
+  function menuButtoOnClick() {
+    itemsOfMenu.forEach(function (item) {
+      item.classList.toggle('display-none');
+    });
+  }
+
+  menuButton.addEventListener('click', menuButtoOnClick);
 })();
+
+// if ((windowInnerWidth < 768) || (windowInnerClientWidth < 768)) {
