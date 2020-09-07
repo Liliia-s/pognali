67aad1f2fc32d3a3af4c2a5ec905d504aebe5ed5
Liliia <befolife@gmail.com>
2020-09-07 23:43:17 +0300

    Внесла правки

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,31 +1,30 @@
-'use strict';
+// 'use strict';
 
-(function () {
-  var itemsOfMenu = document.querySelectorAll('.main-nav__menu, .main-nav__auth, .main-nav__contacts, .main-nav__social');
-  var menuButton = document.querySelector('.main-nav__toggle');
-  // var mainNavAuth = document.querySelector('.main-nav__auth');
-  // var windowInnerWidth = window.innerWidth;
-  // var windowInnerClientWidth = document.body.clientWidth;
-  // var screenWidth = window.screen.width;
-  // var windowOuterWidth = window.outerWidth;
+// (function () {
+//   // var itemsOfMenu = document.querySelectorAll('.main-nav__menu, .main-nav__auth, .main-nav__contacts, .main-nav__social');
+//   var mainNav = document.querySelector('.main-nav');
+//   var menuButton = document.querySelector('.main-nav__toggle');
+//   var mainNavAuth = document.querySelector('.main-nav__auth');
 
-  Array.prototype.slice.call(itemsOfMenu);
+//   // Array.prototype.slice.call(itemsOfMenu);
 
-  // var hideItemsOfMenu = function () {
-  itemsOfMenu.forEach(function (item) {
-    item.classList.toggle('display-none');
-  });
-  // };
+//   function hideMenu() {
+//     mainNav.classList.toggle('main-nav--hide');
+//   };
+//   // if (windowInnerClientWidth >  768) {
+//   //   mainNavAuth.classList.toggle('main-nav--hide');
+//   // }
+//   // }
 
-  // hideItemsOfMenu();
+//   hideMenu();
 
-  function menuButtoOnClick() {
-    itemsOfMenu.forEach(function (item) {
-      item.classList.toggle('display-none');
-    });
-  }
+//   function menuButtoOnClick() {
+//     itemsOfMenu.forEach(function (item) {
+//       item.classList.toggle('main-nav--hide');
+//     });
+//   }
 
-  menuButton.addEventListener('click', menuButtoOnClick);
-})();
+//   menuButton.addEventListener('click', menuButtoOnClick);
+// })();
 
-// if ((windowInnerWidth < 768) || (windowInnerClientWidth < 768)) {
+// // if ((windowInnerWidth < 768) || (windowInnerClientWidth < 768)) {
